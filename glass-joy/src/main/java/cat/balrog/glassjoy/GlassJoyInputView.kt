7a298ce7a4f31362fffe1496f046f00497c8c6e6
Michael Ensly <mensly@mens.ly>
2021-12-30 17:46:33 +1100

    Clear input after swipe gesture

--- glass-joy/src/main/java/cat/balrog/glassjoy/GlassJoyInputView.kt
+++ glass-joy/src/main/java/cat/balrog/glassjoy/GlassJoyInputView.kt
@@ -83,6 +83,7 @@ class GlassJoyInputView @JvmOverloads constructor(
             } else {
                 _inputs.value = if (deltaX > 0) InputGesture.SwipeRight else InputGesture.SwipeLeft
             }
+            _inputs.value = null
             return true
         }
     }
